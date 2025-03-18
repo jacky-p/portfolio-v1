3a0a2aace47029709d0fb2d238892037744fc3e1
Jacqueline <jackyp@berkeley.edu>
2025-03-18 00:14:49 -0700

    filled in some of my info on home & about page

--- src/data/user.js
+++ src/data/user.js
@@ -16,63 +16,38 @@ const INFO = {
 	},
 
 	homepage: {
-		title: "Full-stack web and mobile app developer, and amateur astronaut.",
+		title: "Full-stack software engineer.",
 		description:
-			"I am a backend developer with expertise in Node.js. I have experience in building scalable, secure and reliable web applications using various frameworks and technologies. I enjoy solving complex problems and learning new skills. I am passionate about creating high-quality code that follows best practices and industry standards. I am always looking for new challenges and opportunities to grow as a developer.",
+			"As a recent graduate with a BS in Electrical Engineering and Computer Science, I’ve gained hands-on experience working with various programming languages across different areas of software development. I’m passionate about continuous learning and eager to dive deeper into programming projects as I begin my career as a software engineer. With a strong foundation in both hardware and software, I’m excited to contribute, grow, and take on new challenges in the tech field.",
 	},
 
 	about: {
 		title: "I’m Jacqueline P. I live in California, where I design the future.",
 		description:
-			"I've worked on a variety of projects over the years and I'm proud of the progress I've made. Many of these projects are open-source and available for others to explore and contribute to. If you're interested in any of the projects I've worked on, please feel free to check out the code and suggest any improvements or enhancements you might have in mind. Collaborating with others is a great way to learn and grow, and I'm always open to new ideas and feedback.",
+			"With a BS in Electrical Engineering and Computer Science, I’ve had the opportunity to work with a wide range of programming languages and contribute to various stages of software development. From building applications to troubleshooting and optimizing code, I’ve gained practical experience in both the technical and problem-solving aspects of software engineering. I’m deeply committed to continuous learning, and as I begin my career, I’m excited to take on new challenges and contribute to meaningful projects while further refining my skills as a software engineer.",
+	
 	},
 
 
 	projects: [
 		{
-			title: "Project 1",
+			title: "NEST Meet",
 			description:
-				"Lorem ipsum dolor sit amet. Et incidunt voluptatem ex tempore repellendus qui dicta nemo sit deleniti minima.",
+				"Application that randomly schedules meetings in pairs given a list of indiviuals and applied filters.",
 			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/javascript/javascript.png",
 			linkText: "View Project",
-			link: "https://github.com",
+			link: "https://bizznest.github.io/salinas-bizznest-scheduler/",
 		},
 
 		{
-			title: "Project 2",
+			title: "AI Pacman",
 			description:
-				"Lorem ipsum dolor sit amet. Et incidunt voluptatem ex tempore repellendus qui dicta nemo sit deleniti minima.",
+				"Implementing AI techniques like search algorithms, probabilistic reasoning, and machine learning to design intelligent agents that navigate and interact within the Pac-Man game environment.",
 			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/python/python.png",
 			linkText: "View Project",
 			link: "https://github.com",
 		},
 
-		{
-			title: "Project 3",
-			description:
-				"Lorem ipsum dolor sit amet. Et incidunt voluptatem ex tempore repellendus qui dicta nemo sit deleniti minima.",
-			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/html/html.png",
-			linkText: "View Project",
-			link: "https://github.com",
-		},
-
-		{
-			title: "Project 4",
-			description:
-				"Lorem ipsum dolor sit amet. Et incidunt voluptatem ex tempore repellendus qui dicta nemo sit deleniti minima.",
-			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/javascript/javascript.png",
-			linkText: "View Project",
-			link: "https://github.com",
-		},
-
-		{
-			title: "Project 5",
-			description:
-				"Lorem ipsum dolor sit amet. Et incidunt voluptatem ex tempore repellendus qui dicta nemo sit deleniti minima.",
-			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/javascript/javascript.png",
-			linkText: "View Project",
-			link: "https://github.com",
-		},
 	],
 };
 
