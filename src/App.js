ec77952fd38b944647547595eb5710c1561888e3
Jacqueline <jackyp@berkeley.edu>
2025-04-30 13:48:40 -0700

    hp test 2 index

--- src/App.js
+++ src/App.js
@@ -27,7 +27,7 @@ function App() {
 	return (
 		<div className="App">
 			<Routes>
-				<Route path="/" element={<Homepage />} />
+				<Route index element={<Homepage />} />
 				<Route path="/about" element={<About />} />
 				<Route path="/projects" element={<Projects />} />
 				<Route path="/caro" element={<Caro />} />
